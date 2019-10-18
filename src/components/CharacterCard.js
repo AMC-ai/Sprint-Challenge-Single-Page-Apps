a81fe13a50fb8a587a2d58e439c2558db960f00f
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-10-18 12:29:35 -0400

    setup initialized, char card created, char list api call, all displays on the dom

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from "react";
+import {
+  Card, CardImg, CardBody,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+export default function CharacterCard(props) {
+  return (
+    <div>
+      {/* <span>todo: character</span> */}
+      <Card>
+        <CardBody>
+          <CardTitle>{props.char.name}</CardTitle>
+          <CardSubtitle>{props.char.location.name}</CardSubtitle>
+        </CardBody>
+        <CardImg width="100%" src={props.char.image} alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>{props.char.species}</CardSubtitle>
+          <CardSubtitle>{props.char.status}</CardSubtitle>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
